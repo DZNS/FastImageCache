e7c30eaad474caac713549eddc27718fe1971329
Nikhil Nigade <nikhil@dezinezync.com>
2015-07-31 11:53:22 +0530

    try with framework build.

---
+++ FICFramework/FICFramework/FICFramework.h
@@ -0,0 +1,19 @@
+//
+//  FICFramework.h
+//  FICFramework
+//
+//  Created by Nikhil Nigade on 7/31/15.
+//  Copyright (c) 2015 Dezine Zync Studios LLP. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FICFramework.
+FOUNDATION_EXPORT double FICFrameworkVersionNumber;
+
+//! Project version string for FICFramework.
+FOUNDATION_EXPORT const unsigned char FICFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FICFramework/PublicHeader.h>
+
+#import <FICFramework/DZFICNetworkController.h>
