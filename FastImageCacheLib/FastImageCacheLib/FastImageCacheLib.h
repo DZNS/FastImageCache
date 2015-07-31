dcbc4864bd23f4646c722de1ff33c6c409919f7e
Nikhil Nigade <nikhil@dezinezync.com>
2015-07-31 11:34:45 +0530

    added lib support for carthage.

---
+++ FastImageCacheLib/FastImageCacheLib/FastImageCacheLib.h
@@ -0,0 +1,10 @@
+//
+//  FastImageCacheLib.h
+//  FastImageCacheLib
+//
+//  Created by Nikhil Nigade on 7/31/15.
+//  Copyright (c) 2015 Dezine Zync Studios LLP. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "DZFICNetworkController.h"
